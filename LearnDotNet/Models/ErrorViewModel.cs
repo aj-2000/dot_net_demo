53b429ba58d351fa97a4c84665d1564d47dc86db
aj2000 <hi@ajaysharma.dev>
2023-05-09 20:31:04 +0530

    first commit

---
+++ LearnDotNet/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace LearnDotNet.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
